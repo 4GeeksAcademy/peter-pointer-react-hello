eac2e65f80d4219aae367508a942cd35b10a844f
Pedro Florit Docavo <pedroflorit@msn.com>
2025-01-16 16:57:43 +0000

    desde cero

--- src/js/component/Home.jsx
+++ src/js/component/Home.jsx
@@ -1,8 +1,6 @@
 import React from "react";  // 1. Importo React
 import rigoImage from "../../img/rigo-baby.jpg";  // Include images into your bundle
-import Navbar from "./Navbar.jsx.";
-import { Footer } from "./Footer.jsx";
-import { Jumbotron } from "./Jumbotron.jsx";
+
 
 // 2. Create your first component with PascalCase
 const Home = () => {
@@ -11,10 +9,10 @@ const Home = () => {
 	// 4. Retorno un solo elemento HTML
 	return (
 		<div className="text-center">
-			<Navbar />
+			
 			<h1 className="text-center mt-5">{"Intro React"}</h1>
-			<Jumbotron />
-			{/*
+			
+			
 			<p>
 				<img src={rigoImage} />
 			</p>
@@ -22,8 +20,8 @@ const Home = () => {
 			<a href="#" className="btn btn-success">
 				If you see this green button... bootstrap is working...
 			</a>
-			*/}			
-			<Footer/>
+					
+		
 		</div>
 	);
 };
